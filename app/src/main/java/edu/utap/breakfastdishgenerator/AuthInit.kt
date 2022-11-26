886f60e88f54beb49f984c079c9ea9f3180c0ede
gvidaurri20 <gabrielvidaurri20@gmail.com>
2022-11-25 20:37:50 -0600

    Initial Commit with some code already written

---
+++ app/src/main/java/edu/utap/breakfastdishgenerator/AuthInit.kt
@@ -0,0 +1,57 @@
+package edu.utap.breakfastdishgenerator
+
+import android.content.Intent
+import android.util.Log
+import androidx.activity.result.ActivityResultLauncher
+import com.firebase.ui.auth.AuthUI
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.UserProfileChangeRequest
+
+// https://firebase.google.com/docs/auth/android/firebaseui
+class AuthInit(viewModel: MainViewModel, signInLauncher: ActivityResultLauncher<Intent>) {
+    companion object {
+        private const val TAG = "AuthInit"
+        fun setDisplayName(displayName : String, viewModel: MainViewModel) {
+            Log.d(TAG, "XXX profile change request")
+            // XXX Write me. User is attempting to update display name. Get the profile updates (see android doc)
+
+            Log.d(TAG, "XXX profile change request to name $displayName")
+            val user = FirebaseAuth.getInstance().currentUser
+            val profileUpdates = UserProfileChangeRequest.Builder().setDisplayName(displayName).build()
+            user!!.updateProfile(profileUpdates)
+                .addOnCompleteListener { task ->
+                    if (task.isSuccessful) {
+                        Log.d(TAG, "User profile updated.")
+
+                        user?.reload()
+                        // the below still outputs the previous display name and now the updated displayName
+                        Log.d(TAG, "XXX user changed to ${user?.displayName}")
+                        viewModel.updateUser()
+                    }
+                }
+
+        }
+    }
+
+    init {
+        val user = FirebaseAuth.getInstance().currentUser
+        if(user == null) {
+            Log.d(TAG, "XXX user null")
+            // Choose authentication providers
+            val providers = arrayListOf(
+                AuthUI.IdpConfig.EmailBuilder().build())
+
+            // Create and launch sign-in intent
+            // XXX Write me. Set authentication providers and start sign-in for user
+            val signInIntent = AuthUI.getInstance()
+                .createSignInIntentBuilder()
+                .setAvailableProviders(providers)
+                .setIsSmartLockEnabled(false) // <- ADDED THIS LINE AFTER SUBMISSION SO WILL PROBABLY GET THIS WRONG
+                .build()
+            signInLauncher.launch(signInIntent)
+        } else {
+            Log.d(TAG, "XXX user ${user.displayName} email ${user.email}")
+            viewModel.updateUser()
+        }
+    }
+}
---
+++ app/src/main/java/edu/utap/breakfastdishgenerator/MainActivity.kt
@@ -0,0 +1,118 @@
+package edu.utap.breakfastdishgenerator
+
+import android.os.Bundle
+import com.google.android.material.snackbar.Snackbar
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.navigateUp
+import androidx.navigation.ui.setupActionBarWithNavController
+import android.view.Menu
+import android.view.MenuItem
+import androidx.activity.viewModels
+import com.firebase.ui.auth.AuthUI
+import com.firebase.ui.auth.FirebaseAuthUIActivityResultContract
+import com.google.firebase.auth.FirebaseAuth
+import edu.utap.breakfastdishgenerator.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private val viewModel: MainViewModel by viewModels()
+    private lateinit var binding : ActivityMainBinding
+
+    // See: https://developer.android.com/training/basics/intents/result
+    private val signInLauncher =
+        registerForActivityResult(FirebaseAuthUIActivityResultContract()) {
+            viewModel.updateUser()
+        }
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        if(savedInstanceState == null) {
+            binding.displayNameET.text.clear()
+        }
+
+        // XXX Write me. Set data to display in UI
+        viewModel.observeDisplayName().observe(this, Observer { binding.userName.text = viewModel.observeDisplayName().value })
+        viewModel.observeEmail().observe(this, Observer { binding.userEmail.text = viewModel.observeEmail().value })
+        viewModel.observeUid().observe(this, Observer{ binding.userUid.text = viewModel.observeUid().value })
+
+        binding.logoutBut.setOnClickListener {
+            // XXX Write me.
+            viewModel.signOut()
+
+            binding.userName.text = viewModel.observeDisplayName().value
+            binding.userEmail.text = viewModel.observeEmail().value
+            binding.userUid.text = viewModel.observeUid().value
+        }
+        binding.loginBut.setOnClickListener {
+            // XXX Write me.
+            val user = FirebaseAuth.getInstance().currentUser
+            if(user == null) {
+                val providers = arrayListOf(
+                    AuthUI.IdpConfig.EmailBuilder().build())
+
+                val signInIntent = AuthUI.getInstance()
+                    .createSignInIntentBuilder()
+                    .setAvailableProviders(providers)
+                    .build()
+                signInLauncher.launch(signInIntent)
+            }
+        }
+        binding.setDisplayName.setOnClickListener {
+            // XXX Write me.
+            if(binding.displayNameET.text.toString() != "")
+                AuthInit.setDisplayName(binding.displayNameET.text.toString(), viewModel)
+            binding.displayNameET.text.clear()
+        }
+
+        AuthInit(viewModel, signInLauncher)
+    }
+
+
+    /*private lateinit var appBarConfiguration: AppBarConfiguration
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        setSupportActionBar(binding.toolbar)
+
+        val navController = findNavController(R.id.nav_host_fragment_content_main)
+        appBarConfiguration = AppBarConfiguration(navController.graph)
+        setupActionBarWithNavController(navController, appBarConfiguration)
+
+        binding.fab.setOnClickListener { view ->
+            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                .setAction("Action", null).show()
+        }
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        menuInflater.inflate(R.menu.menu_main, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        return when (item.itemId) {
+            R.id.action_settings -> true
+            else -> super.onOptionsItemSelected(item)
+        }
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        val navController = findNavController(R.id.nav_host_fragment_content_main)
+        return navController.navigateUp(appBarConfiguration)
+                || super.onSupportNavigateUp()
+    }
+    */
+
+}
